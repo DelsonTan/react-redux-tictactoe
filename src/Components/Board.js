a79b49bca43b84f90d49183ac9f3a6db8e1d0790
Delson Tan <delsonwtan@gmail.com>
2018-09-11 20:10:03 -0700

    Get basic appearance of game board down

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,11 +2,29 @@ import React, {Component} from 'react'
 import Square from './Square'
 
 class Board extends Component {
+  renderSquare() {
+    return <Square />
+  }
+
   render() {
     return (
       <div className="Board">
         Board!
-        <Square />
+        <div>
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+        </div>
+        <div>
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+        </div>
+        <div>
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+        </div>
       </div>
     )
   }
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 
 class Square extends Component {
   render() {
-    return <div className="Square">Square!</div>
+    return <button className="Square">X</button>
   }
 }
 
