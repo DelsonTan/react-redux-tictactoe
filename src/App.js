570e6799925c500d18d6e2f1955c2fa5edd065a7
Delson Tan <delsonwtan@gmail.com>
2018-09-15 23:20:04 -0700

    Remove logo and centre all content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 import Board from './containers/Board'
 import Score from './components/Score'
@@ -9,21 +7,11 @@ import Notification from './components/Notification'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Tic-Tac-Toe</h1>
-        </header>
-        <p className="App-intro">
-          The unwinnable game{' '}
-          <span role="img" aria-label="Smirk">
-            😏
-          </span>
-        </p>
+      <center className="App">
         <Board />
         <Notification />
         <Score />
-      </div>
+      </center>
     )
   }
 }
